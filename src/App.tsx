8bd5d3817f7636cbd27bb943c28813550ba76d8a
Giovanni Ferron <gvnnrulez@gmail.com>
2020-04-02 09:57:42 +1100

    Add spectre.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.tsx</code> and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
+  <div className="container">
+    <div className="columns">
+      <div className="column">
+        <h1>dnd-tavern</h1>
+      </div>
+    </div>
   </div>
 );
 
