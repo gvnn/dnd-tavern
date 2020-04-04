8931effc8637f9e772f0b904102a00899c92751e
Giovanni Ferron <gvnnrulez@gmail.com>
2020-04-04 13:26:05 +1100

    Add disconnect

--- src/components/launchPeer.tsx
+++ src/components/launchPeer.tsx
@@ -3,25 +3,56 @@ import Peer from 'peerjs';
 import { useTavern } from '../state/tavern';
 
 export const LaunchPeer = () => {
-  const { dispatch } = useTavern();
+  const { state, dispatch } = useTavern();
+
   const [connectButtonState, setConnectButtonState] = useState('Connect');
+  const [disconnectButtonState, setDisconnectButtonState] = useState(
+    'Disconnect',
+  );
 
   const connect = () => {
     dispatch({ type: 'CONNECT' });
     setConnectButtonState('Connecting...');
-    new Peer({
+
+    const peer = new Peer({
       key: 'beer',
       host: 'dnd-tavern-server.herokuapp.com',
       secure: true,
-    }).on('open', (brokerId) => {
+    });
+
+    peer.on('disconnected', () => {
+      dispatch({ type: 'DISCONNECTED' });
+      setConnectButtonState('Connect');
+      setDisconnectButtonState('Disconnect');
+    });
+
+    peer.on('open', (brokerId) => {
       dispatch({ type: 'CONNECTED', brokerId });
       setConnectButtonState('Connected');
     });
+
+    dispatch({ type: 'SET_PEER', peerInstance: peer });
   };
 
-  return (
+  const disconnect = () => {
+    setDisconnectButtonState('Disconnecting...');
+    dispatch({ type: 'DISCONNECT' });
+    state.peerInstance?.destroy();
+  };
+
+  const renderConnect = () => (
     <button className="btn" onClick={connect}>
       {connectButtonState}
     </button>
   );
+
+  const renderDisconnect = () => (
+    <button className="btn" onClick={disconnect}>
+      {disconnectButtonState}
+    </button>
+  );
+
+  return state.connectionStatus === 'CONNECTED'
+    ? renderDisconnect()
+    : renderConnect();
 };
--- src/state/tavernStateReducer.ts
+++ src/state/tavernStateReducer.ts
@@ -1,23 +1,45 @@
+import Peer from 'peerjs';
+
 interface TavernState {
   connectionStatus: string;
   brokerId: string | null;
+  peerInstance: Peer | null;
 }
 
 export const initialState: TavernState = {
   connectionStatus: 'NOT_CONNECTED',
   brokerId: null,
+  peerInstance: null,
 };
 
 export interface ConnectedAction {
   type: 'CONNECTED';
-  brokerId: string | null;
+  brokerId: string;
 }
 
 export interface ConnectAction {
   type: 'CONNECT';
 }
 
-type ReducerActions = ConnectedAction | ConnectAction;
+export interface DisconnectAction {
+  type: 'DISCONNECT';
+}
+
+export interface DisconnectedAction {
+  type: 'DISCONNECTED';
+}
+
+export interface SetPeerAction {
+  type: 'SET_PEER';
+  peerInstance: Peer;
+}
+
+type ReducerActions =
+  | ConnectedAction
+  | ConnectAction
+  | DisconnectAction
+  | DisconnectedAction
+  | SetPeerAction;
 
 export interface TavernStateContext {
   state: TavernState;
@@ -29,8 +51,14 @@ export const tavernStateReducer = (
   action: ReducerActions,
 ) => {
   switch (action.type) {
+    case 'SET_PEER':
+      return { ...state, peerInstance: action.peerInstance };
     case 'CONNECT':
       return { ...state, connectionStatus: 'CONNECTING' };
+    case 'DISCONNECT':
+      return { ...state, connectionStatus: 'DISCONNECTING' };
+    case 'DISCONNECTED':
+      return initialState;
     case 'CONNECTED':
       return {
         ...state,
